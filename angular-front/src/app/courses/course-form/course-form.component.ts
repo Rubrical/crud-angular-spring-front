7c8e0c28255a4cbecd736fb917a557734d6aa37d
Filipe <filipesalviano@proton.me>
2024-03-07 08:52:20 -0300

    Form types refactor

--- angular-front/src/app/courses/course-form/course-form.component.ts
+++ angular-front/src/app/courses/course-form/course-form.component.ts
@@ -1,8 +1,8 @@
+import { Location } from "@angular/common";
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {CoursesService} from "../services/courses.service";
-import {MatSnackBar} from "@angular/material/snack-bar";
-import {Location} from "@angular/common";
+import { NonNullableFormBuilder } from "@angular/forms";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { CoursesService } from "../services/courses.service";
 
 @Component({
   selector: 'app-course-form',
@@ -11,15 +11,18 @@ import {Location} from "@angular/common";
 })
 export class CourseFormComponent {
 
-  form: FormGroup;
+  form = this.formBuilder.group({
+    name: [''], 
+    category: ['']
+  });
 
   constructor(
-    private formBuilder: FormBuilder,
+    private formBuilder: NonNullableFormBuilder,
     private courseService: CoursesService,
     private snackBar: MatSnackBar,
     private location: Location
   ) {
-    this.form = this.formBuilder.group({name: [null], category: [null]})
+    
   }
 
   onSubmit() {
